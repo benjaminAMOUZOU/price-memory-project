bc454e297feccdad26f169dff0cf7f389068a0a2
Benjamin kokou AMOUZOU <benkokouamouzou@gmail.com>
2020-11-10 01:41:07 +0000

    Ajout d'une barre d'état

---
+++ PriceMemory/app/src/main/java/com/benjamin/pricememory/view/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.benjamin.pricememory.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.benjamin.pricememory.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
